eb5940a8e42558089cf1bfeb069b6787ad4e2c86
Robert Jackson <rjackson@linkedin.com>
2020-05-08 13:29:57 -0400

    yarn lint --fix

--- check-suite-complete/index.js
+++ check-suite-complete/index.js
@@ -1,19 +1,18 @@
 // https://docs.microsoft.com/en-us/azure/azure-functions/functions-bindings-http-webhook-trigger?tabs=javascript
 
 module.exports = async function (context, req) {
-  context.log("JavaScript HTTP trigger function processed a request.");
+  context.log('JavaScript HTTP trigger function processed a request.');
 
   if (req.query.name || (req.body && req.body.name)) {
     context.bindings.msg = `ohai ${req.query.name || req.body.name}`;
     context.res = {
       // status: 200, /* Defaults to 200 */
-      body: "Hello " + (req.query.name || req.body.name),
+      body: 'Hello ' + (req.query.name || req.body.name),
     };
   } else {
     context.res = {
       status: 400,
-      body: "Please pass a name on the query string or in the request body",
+      body: 'Please pass a name on the query string or in the request body',
     };
   }
 };
-
